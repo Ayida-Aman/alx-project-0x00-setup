84b901bccd92b0035e55c2aebd419747c6aa3e7a
Ayida-Aman <amanayida1@gmail.com>
2025-07-20 20:34:13 +0300

    add

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BtnProps } from '@/interfaces'
+function Button({title}) {
+  return (
+    <>
+        <button className='' >
+            {title} 
+        </button>
+    </>
+  )
+}
+
+export default Button
--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,9 @@
 export interface PillProps {
   title: string
 }
+
+export interface BtnProps {
+  title: string
+  sizes: string
+  shapes:string
+}
